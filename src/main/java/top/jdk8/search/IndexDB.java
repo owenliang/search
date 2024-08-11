350d6da0a27eb323b6c37ee0745bde3400092208
qianyu.ld <qianyu.ld@alibaba-inc.com>
2024-08-11 19:02:25 +0800

    update

--- src/main/java/top/jdk8/search/IndexDB.java
+++ src/main/java/top/jdk8/search/IndexDB.java
@@ -2,7 +2,10 @@ package top.jdk8.search;
 
 import org.rocksdb.Options;
 import org.rocksdb.RocksDB;
+import org.xerial.snappy.Snappy;
 import top.jdk8.search.protobuf.SearchProto;
+
+import java.nio.charset.StandardCharsets;
 
 
 public class IndexDB {
@@ -17,15 +20,19 @@ public class IndexDB {
     }
 
     // 正排
-    public void addDocument(String docId, SearchProto.Document document) {
-
+    public void addDocument(String docId, SearchProto.Document document) throws Exception {
+        byte[] bytes = document.toByteArray();
+        // System.out.println(bytes.length);
+        db.put(docId.getBytes(StandardCharsets.UTF_8), Snappy.compress(bytes));
     }
-    public SearchProto.Document getDocument(String docId) {
-        return null;
+    public SearchProto.Document getDocument(String docId) throws Exception {
+        byte[] bytes =db.get(docId.getBytes(StandardCharsets.UTF_8));
+        // System.out.println(bytes.length);
+        return SearchProto.Document.parseFrom(Snappy.uncompress(bytes));
     }
 
     // 倒排
-    public void addPostingList(SearchProto.PostingList postingList) {
+    public void addPostingList(String term,SearchProto.PostingList postingList) {
 
     }
     public SearchProto.PostingList getPostingList(String term) {
--- src/main/java/top/jdk8/search/Main.java
+++ src/main/java/top/jdk8/search/Main.java
@@ -1,8 +1,31 @@
 package top.jdk8.search;
 
+import top.jdk8.search.protobuf.SearchProto;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentSkipListMap;
+
 public class Main {
     public static void main(String[] args) throws Exception {
         String dbpath=args[0];
         IndexDB db = new IndexDB(dbpath);
+
+        ////////// 写入
+        SearchProto.Document.Builder docBuilder = SearchProto.Document.newBuilder();
+        docBuilder.setContent("hello world");
+
+        SearchProto.TermInfo.Builder termInfoBuilder = SearchProto.TermInfo.newBuilder();
+        termInfoBuilder.setTerm("你好").setOffset(5).setLength(2);
+        docBuilder.addTerms((termInfoBuilder.build()));
+        SearchProto.Document document=docBuilder.build();
+
+        db.addDocument("1",document);
+
+        ////////// 查询
+        SearchProto.Document doc = db.getDocument("1");
+        System.out.println(doc);
+
+        ///////// 倒排表（doc_id -> PostingItem）
     }
 }
